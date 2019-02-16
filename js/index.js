27f59ca9560798010591c8c471b0c452df113a3a
SestoAle <alessandrosestini92@gmail.com>
2019-02-16 20:03:28 +0100

    Mobile version

--- js/index.js
+++ js/index.js
@@ -2,21 +2,43 @@
 $(document).ready(function(){
 	// Load about content
 	content_loader("about/about.html", "#about"); 
+	content_loader("about/about.html", "#about-mobile"); 
 
 	// Initialize all the href
 	$('#about').click(function()
 	{
 		content_loader('about/about.html', this)
 	})
+	$('#about-mobile').click(function()
+	{
+		content_loader('about/about.html', this)
+	})
 
 	$('#title-name').click(function()
 	{
-		content_loader('about/about.html', this)
+		content_loader('about/about.html', "#about")
+	})
+	$('#title-name-mobile').click(function()
+	{
+		content_loader('about/about.html', "#about-mobile")
 	})
 
 	$('#projects').click(function()
 	{
 		content_loader('projects/projects.html', this)
+	})
+	$('#projects-mobile').click(function()
+	{
+		content_loader('projects/projects.html', this)
+	})
+
+	$('#education').click(function()
+	{
+		content_loader('education/education.html', this)
+	})
+	$('#education-mobile').click(function()
+	{
+		content_loader('education/education.html', this)
 	})
 })
 
